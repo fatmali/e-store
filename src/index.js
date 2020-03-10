daab2741c15c33475283aa60caa958d2152c6b5b
Fatma Ali <alifatma2019@gmail.com>
2020-03-10 11:24:18 +0300

    fixed basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="e-store">
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
