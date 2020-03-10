6a0856e9a2f854f9222974dad1d36c2b7b10ee10
Fatma Ali <alifatma2019@gmail.com>
2020-03-10 11:13:45 +0300

    Updates

---
+++ precache-manifest.f515484281b651b2cc9cb2cd12b60e5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda2697457ba0b5414627184ef278817",
+    "url": "/e-store/index.html"
+  },
+  {
+    "revision": "ddad32cf67882af7cb3c",
+    "url": "/e-store/static/css/main.fd3d1d9e.chunk.css"
+  },
+  {
+    "revision": "18c9cdb68dd34a923c59",
+    "url": "/e-store/static/js/2.03687a35.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/e-store/static/js/2.03687a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddad32cf67882af7cb3c",
+    "url": "/e-store/static/js/main.0266803d.chunk.js"
+  },
+  {
+    "revision": "5f4b9a6717ad943d7acb",
+    "url": "/e-store/static/js/runtime-main.31608005.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-store/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
